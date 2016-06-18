66d0350169c0b4d17619e1352eebf2efca595475
Lucas Ortigara Reis <luucasor@gmail.com>
2016-06-18 18:41:31 -0300

    Update web.pp

--- manifests/web.pp
+++ manifests/web.pp
@@ -2,7 +2,7 @@ exec { "apt-update":
    command => "/usr/bin/apt-get update"
 }
 
-package { ["openjdk-7-jre", "tomcat7"]
+package { ["openjdk-7-jre", "tomcat7"]:
    ensure => installed,
    require => Exec["apt-update"]
 }
